d4c144c380c9861cd39f1f40a1d4c9b3601184e4
AbdGhordlo <abdallahghordlo@gmail.com>
2026-05-03 18:01:21 +0300

    cart front end progress

--- app/(tabs)/cart.tsx
+++ app/(tabs)/cart.tsx
@@ -1,11 +1,25 @@
+import CustomHeader from "@/components/CustomHeader";
+import { useCartStore } from "@/store/cart.store";
 import React from "react";
-import { Text, View } from "react-native";
+import { FlatList, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Cart = () => {
+  const { items, getTotalItems, getTotalPrice } = useCartStore();
+
+  const totalItems = getTotalItems();
+  const totalPrice = getTotalPrice();
+
   return (
-    <View>
-      <Text>Cart</Text>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <FlatList
+        data={items}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <Text>Cart Item</Text>}
+        contentContainerClassName="pb-28 px-5 pt-5"
+        ListHeaderComponent={() => <CustomHeader title="Your Cart" />}
+      />
+    </SafeAreaView>
   );
 };
 
