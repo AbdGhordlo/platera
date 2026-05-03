f2aa462aad41b12132430a8d26229f6f3de0bb56
AbdGhordlo <abdallahghordlo@gmail.com>
2026-05-03 17:28:43 +0300

    Added Search Functionality & Search Bar

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,5 +1,7 @@
 import CartButton from "@/components/CartButton";
+import Filter from "@/components/Filter";
 import MenuCard from "@/components/MenuCard";
+import SearchBar from "@/components/SearchBar";
 import { getCategories, getMenu } from "@/lib/appwrite";
 import useAppwrite from "@/lib/useAppwrite";
 import { MenuItem } from "@/type";
@@ -73,7 +75,9 @@ const Search = () => {
               <CartButton />
             </View>
 
-            <Text>Search Input</Text>
+            <SearchBar />
+
+            <Filter categories={categories!} />
 
             <Text>Filter</Text>
           </View>
